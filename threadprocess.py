466363ba356d6cfaff580a394a2644298c0190df
Liberbell <liberbell@gmail.com>
2020-10-29 08:54:46 +0900

    threadprocess3

--- threadprocess.py
+++ threadprocess.py
@@ -4,3 +4,7 @@ import multiprocessing
 def square(numbers):
     for n in numbers:
         print("Square of %d is %d" %(n, n * n))
+
+def cube(numbers):
+    for n in numbers:
+        print("Cube of %d is %")
