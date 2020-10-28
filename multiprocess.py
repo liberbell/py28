80b13783dd6264ebc63ae6733b46eac3bb75f119
Liberbell <liberbell@gmail.com>
2020-10-28 09:45:31 +0900

    multiprocess4

--- multiprocess.py
+++ multiprocess.py
@@ -5,3 +5,9 @@ def square(number):
     time.sleep(1)
     result = number * number
     print("The number %d squares to %d " %(number, result))
+
+numbers = [1, 2, 3, 4]
+start_time = time.time()
+
+for number in numbers:
+    square(number)
