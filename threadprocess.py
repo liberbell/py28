2ffc6a097d0e86d466b553049e0252a6e4ea4c7b
Liberbell <liberbell@gmail.com>
2020-10-30 17:51:57 +0900

    threadprocess2

--- threadprocess.py
+++ threadprocess.py
@@ -1,26 +1,26 @@
 import time
 import multiprocessing
 
-def square(numbers):
-    for n in numbers:
-        print("Square of %d is %d" %(n, n * n))
+# def square(numbers):
+#     for n in numbers:
+#         print("Square of %d is %d" %(n, n * n))
 
-def cube(numbers):
-    for n in numbers:
-        print("Cube of %d is %d" %(n, n * n * n))
+# def cube(numbers):
+#     for n in numbers:
+#         print("Cube of %d is %d" %(n, n * n * n))
 
-num_list = [2, 3, 8]
+# num_list = [2, 3, 8]
 
-p1 = multiprocessing.Process(target=square, args=(num_list, ))
-p2 = multiprocessing.Process(target=cube, args=(num_list, ))
+# p1 = multiprocessing.Process(target=square, args=(num_list, ))
+# p2 = multiprocessing.Process(target=cube, args=(num_list, ))
 
-p1.start()
-p2.start()
+# p1.start()
+# p2.start()
 
-p1.join()
-p2.join()
+# p1.join()
+# p2.join()
 
-print("\nCompleted")
+# print("\nCompleted")
 
 square_result = []
 def square(numbers):
