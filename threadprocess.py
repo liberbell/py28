d8c3c2bdcfb50f890763948614335b7a8e5c54a9
Liberbell <liberbell@gmail.com>
2020-10-30 17:58:19 +0900

    threadprocess6

--- threadprocess.py
+++ threadprocess.py
@@ -23,18 +23,22 @@ num_list = [2, 3, 8]
 # print("\nCompleted")
 
 square_result = []
+# def square(numbers):
+
+#     global square_result
+
+#     for n in numbers:
+#         print("Square of %d is %d " %(n, n * n))
+#         square_result.append(n * n)
+
+# p1 = multiprocessing.Process(target=square, args=(num_list, ))
+
+# p1.start()
+# p1.join()
+
+# print("\nResult: ", square_result)
+# print("\nCompleted.")
+
 def square(numbers):
 
     global square_result
-
-    for n in numbers:
-        print("Square of %d is %d " %(n, n * n))
-        square_result.append(n * n)
-
-p1 = multiprocessing.Process(target=square, args=(num_list, ))
-
-p1.start()
-p1.join()
-
-print("\nResult: ", square_result)
-print("\nCompleted.")
