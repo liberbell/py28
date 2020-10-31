39f65dd79d7415334aa79599927fd4337cc677e0
Liberbell <liberbell@gmail.com>
2020-10-31 16:44:16 +0900

    threadprocess5

--- threadprocess.py
+++ threadprocess.py
@@ -10,7 +10,7 @@ import threading
 #     for n in numbers:
 #         print("Cube of %d is %d" %(n, n * n * n))
 
-num_list = [2, 3, 8]
+num_list = [2, 3, 8, 16]
 
 # p1 = multiprocessing.Process(target=square, args=(num_list, ))
 # p2 = multiprocessing.Process(target=cube, args=(num_list, ))
