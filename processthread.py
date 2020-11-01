2d48aae24035f39d1b0b72d4a0c506369ad95bc9
Liberbell <liberbell@gmail.com>
2020-11-01 13:22:42 +0900

    processsquare8

--- processthread.py
+++ processthread.py
@@ -24,17 +24,24 @@ num_list = [2, 3, 8]
 
 square_result = []
 
+# def square(numbers):
+#     global square_result
+
+#     for n in numbers:
+#         print("Square of %d is %d" %(n, n * n))
+#         square_result.append(n * n)
+
+# p1 = multiprocessing.Process(target=square, args=(num_list, ))
+
+# p1.start()
+# p1.join()
+
+# print("\nReuslt: ", square_result)
+# print("\nComplited")
+
 def square(numbers):
     global square_result
 
     for n in numbers:
         print("Square of %d is %d" %(n, n * n))
         square_result.append(n * n)
-
-p1 = multiprocessing.Process(target=square, args=(num_list, ))
-
-p1.start()
-p1.join()
-
-print("\nReuslt: ", square_result)
-print("\nComplited")
