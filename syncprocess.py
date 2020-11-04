37384f549275e7bb348b79687573a1870beea66a
Liberbell <liberbell@gmail.com>
2020-11-04 09:49:00 +0900

    syncprocess10

--- syncprocess.py
+++ syncprocess.py
@@ -23,3 +23,10 @@ print(balance)
 
 balance = withdraw_without_mp(balance, 5)
 print(balance)
+
+def deposit_without_lock(balance, amount):
+    for i in range(100):
+        time.sleep(0.01)
+        balance.value += amount
+
+    return  balance
