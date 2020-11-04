5d954c2ad61e3591913f569d6868e04633232f59
Liberbell <liberbell@gmail.com>
2020-11-04 09:45:10 +0900

    syncproicess8

--- syncprocess.py
+++ syncprocess.py
@@ -1,7 +1,7 @@
 import time
 import multiprocessing
 
-def deposit_with_mp(balance, amount):
+def deposit_without_mp(balance, amount):
     for i in range(100):
         time.sleep(0.01)
         balance += amount
@@ -18,7 +18,7 @@ def withdraw_without_mp(balance, amount):
 balance = 600
 print(balance)
 
-print(deposit_with_mp(balance, 5))
+print(deposit_without_mp(balance, 5))
 
 balance = withdraw_without_mp(balance, 5)
 print(balance)
