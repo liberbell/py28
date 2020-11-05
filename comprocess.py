bc4424e4a0ce7c144808da9ce034f54bb09d755f
Liberbell <liberbell@gmail.com>
2020-11-05 09:36:54 +0900

    comprocess2

---
+++ comprocess.py
@@ -0,0 +1,10 @@
+import multiprocessing
+
+def is_even(numbers, q):
+    for n in numbers:
+        if n % 2 == 0:
+            q.put(n)
+
+def print_numbers(q):
+    while not q.empty():
+        print(q.get())
